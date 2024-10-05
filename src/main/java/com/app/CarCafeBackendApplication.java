1cd03e0f476d1f12bf4811e5301941a8f29afe5a
NihalAhmad34 <nihalahmad.a26@gmail.com>
2024-10-05 23:58:23 +0530

    initial commit

---
+++ src/main/java/com/app/CarCafeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCafeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCafeBackendApplication.class, args);
+	}
+
+}
